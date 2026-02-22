ca5f901c3b559f02bd2405e4902a6cdd1eef2225
Ethan Sitterly <Ethan.sitterly@gmail.com>
2026-02-22 15:57:53 -0600

    React Back End Services.05

--- medals-api/obj/Debug/net8.0/Medals-Api.AssemblyInfo.cs
+++ medals-api/obj/Debug/net8.0/Medals-Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Medals-Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8d5409c821e1323927ce15a978a3fcce11bac2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f52f6a7dc22abbe67ee30ce6714a2bf1ddf01514")]
 [assembly: System.Reflection.AssemblyProductAttribute("Medals-Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Medals-Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
